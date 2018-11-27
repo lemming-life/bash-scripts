e1c76b9ac33548d286c660971bfd0b2004839e10
lemming-life <email@lemming.life>
2018-11-26 21:56:30 -0700

    Improved uninstall mongo script.

--- uninstall/mongo.sh
+++ uninstall/mongo.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
 REMOVE_MONGO_DATA=${REMOVE_MONGO_DATA:-false}
+MONGO_VERSION=${MONGO_VERSION:-}
 
-sudo service mongodb stop
-sudo apt-get purge -y mongodb*
-
-if [ $REMOVE_MONGO_DATA = true ]; then
-  echo "Removing Mongo data."
-  sudo rm -r /var/log/mongodb
-  sudo rm -r /var/lib/mongodb
+if [ "$(uname)" = "Darwin" ]; then
+  if [ -z $MONGO_VERSION ]; then
+    brew uninstall mongo
+  else
+    brew uninstall mongo@$MONGO_VERSION
+  fi
 else
-  echo "Leaving Mongo data. Set REMOVE_MONGO_DATA=true when calling script to remove data."
+  sudo service mongodb stop
+  sudo apt-get purge -y mongodb*
+
+  if [ $REMOVE_MONGO_DATA = true ]; then
+    echo "Removing Mongo data."
+    sudo rm -r /var/log/mongodb
+    sudo rm -r /var/lib/mongodb
+  else
+    echo "Leaving Mongo data. Set REMOVE_MONGO_DATA=true when calling script to remove data."
+  fi
 fi
